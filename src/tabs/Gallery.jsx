6a61b270c82e5a6f37465cea6487b16755733c3e
Viktoria <viktoria.ttravel@gmail.com>
2023-08-06 12:33:29 +0300

    Update Gallery.jsx

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -7,13 +7,25 @@ export const Gallery = () => {
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState('');
   const [showBtn, setShowBtn] = useState(false);
+  const [isEmpty, setIsEmpty] = useState(false);
+  const [error, setError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     if (!query) return;
-    getImages(query, page).then(({ photos, total_results }) => {
-      setImages(prevState => [...prevState, ...photos]);
-      setShowBtn(page < Math.ceil(total_results / 15));
-    });
+    setIsLoading(true);
+    getImages(query, page)
+      .then(({ photos, total_results }) => {
+        if (photos.length === 0) {
+          setIsEmpty(true);
+        }
+        setImages(prevState => [...prevState, ...photos]);
+        setShowBtn(page < Math.ceil(total_results / 15));
+      })
+      .catch(error => {
+        setError(error.message);
+      })
+      .finally(setIsLoading(false));
   }, [query, page]);
 
   const onSubmit = value => {
@@ -21,6 +33,8 @@ export const Gallery = () => {
     setPage(1);
     setShowBtn(false);
     setQuery(value);
+    setIsEmpty(false);
+    setError(false);
   };
   const onClickLoadMore = () => {
     setPage(prevState => prevState + 1);
@@ -39,6 +53,11 @@ export const Gallery = () => {
         ))}
       </Grid>
       {showBtn && <Button onClick={onClickLoadMore}>Load more</Button>}
+      {isEmpty && (
+        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+      )}
+      {error && <Text textAlign="center">Sorry. {error} 😭</Text>}
+      {isLoading && <p>Loading...</p>}
     </>
   );
 };
